a99a97142b69d016827e7df93c0b13b4ded3e23f
Shraddha Singh <133136318+shrxddhx@users.noreply.github.com>
2023-09-26 23:31:44 +0530

    Scatter 3d plots

---
+++ scatter1.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+#Single Points
+ax=plt.axes(projection="3d")  #the type of projection
+ax.scatter(3,5,7)  #plotting x,y,z
+plt.show()
